c66ab64cf399e5b38f3f21609dee29ad88f7d6f0
vinitSanctumcloud <vinit@sanctumcloud.com>
2025-07-09 14:02:03 +0530

    data

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 import { Header } from '@/components/header'
 import { Footer } from '@/components/footer'
 import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+// import Link from 'next/link'
 import Image from 'next/image'
 import { 
   ArrowRight, 
